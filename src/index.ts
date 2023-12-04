313a36513971a6ff2087e80794ab054ebec0a6d0
fruitbox12 <dylanwong007@gmail.com>
2023-12-04 03:41:17 -0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,144 +1,115 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
-import * as k8s from "@kubernetes/client-node";
-import * as fs from "fs";
-
-//process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = "0";
-const MEMCACHED_GROUP = "cache.example.com";
-const MEMCACHED_VERSION = "v1";
-const MEMCACHED_PLURAL = "memcacheds";
-const NAMESPACE = "ts-operator";
-
-interface MemcachedSpec {
-  size: number;
-}
-interface MemcachedStatus {
-  pods: string[];
-}
-interface Memcached {
-  apiVersion: string;
-  kind: string;
-  metadata: k8s.V1ObjectMeta;
-  spec?: MemcachedSpec;
-  status?: MemcachedStatus;
-}
+import * as k8s from '@kubernetes/client-node';
 
 const kc = new k8s.KubeConfig();
 kc.loadFromDefault();
 
-const k8sApi = kc.makeApiClient(k8s.AppsV1Api);
-const k8sApiMC = kc.makeApiClient(k8s.CustomObjectsApi);
-const k8sApiPods = kc.makeApiClient(k8s.CoreV1Api);
-
-const deploymentTemplate = fs.readFileSync("memcached-deployment.json", "utf-8");
+const k8sApiCustom = kc.makeApiClient(k8s.CustomObjectsApi);
 const watch = new k8s.Watch(kc);
 
-async function onEvent(phase: string, apiObj: any) {
-  log(`Received event in phase ${phase}.`);
-  if (phase == "ADDED") {
-    scheduleReconcile(apiObj);
-  } else if (phase == "MODIFIED") {
-    scheduleReconcile(apiObj);
-  } else if (phase == "DELETED") {
-    await deleteResource(apiObj);
-  } else {
-    log(`Unknown event type: ${phase}`);
-  }
+// Define the Fireline interfaces
+interface FirelineSpec {
+  stages: string[];
 }
 
-async function deleteResource(obj: Memcached) {
-  log(`Deleted ${obj.metadata.name}`);
-  return k8sApi.deleteNamespacedDeployment(obj.metadata.name!, NAMESPACE);
+interface StageDetail {
+  stageName: string;
+  startTime: string;
+  endTime?: string;
+  status: string; // 'InProgress', 'Completed', 'Failed'
 }
 
-function onDone(err: any) {
-  log(`Connection closed. ${err}`);
-  watchResource();
+interface FirelineStatus {
+  currentStage: string;
+  stageDetails: StageDetail[];
 }
 
-async function watchResource(): Promise<any> {
-  log("Watching API");
-  return watch.watch(
-    `/apis/${MEMCACHED_GROUP}/${MEMCACHED_VERSION}/namespaces/${NAMESPACE}/${MEMCACHED_PLURAL}`,
-    {},
-    onEvent,
-    onDone,
-  );
+interface Fireline {
+  apiVersion: string;
+  kind: string;
+  metadata: k8s.V1ObjectMeta;
+  spec: FirelineSpec;
+  status?: FirelineStatus;
 }
 
-let reconcileScheduled = false;
-
-function scheduleReconcile(obj: Memcached) {
-  if (!reconcileScheduled) {
-    setTimeout(reconcileNow, 1000, obj);
-    reconcileScheduled = true;
-  }
-}
-
-async function reconcileNow(obj: Memcached) {
-  reconcileScheduled = false;
-  const deploymentName: string = obj.metadata.name!;
-  //check if deployment exists. Create it if it doesn't.
+// Function to watch Fireline resources
+async function watchFirelineResource(): Promise<string> {
   try {
-    const response = await k8sApi.readNamespacedDeployment(deploymentName, NAMESPACE);
-    //patch the deployment
-    const deployment: k8s.V1Deployment = response.body;
-    deployment.spec!.replicas = obj.spec!.size;
-    k8sApi.replaceNamespacedDeployment(deploymentName, NAMESPACE, deployment);
+    await watch.watch(
+      '/apis/tekton.dev/v1/namespaces/default/firelines',
+      {},
+      onFirelineEvent,
+      () => watchFirelineResource()
+    );
+    return 'Started watching Fireline resources.';
   } catch (err) {
-    //Create the deployment
-    const newDeployment: k8s.V1Deployment = JSON.parse(deploymentTemplate);
-    newDeployment.metadata!.name = deploymentName;
-    newDeployment.spec!.replicas = obj.spec!.size;
-    newDeployment.spec!.selector!.matchLabels!["deployment"] = deploymentName;
-    newDeployment.spec!.template!.metadata!.labels!["deployment"] = deploymentName;
-    k8sApi.createNamespacedDeployment(NAMESPACE, newDeployment);
+    return `Error watching Fireline resources: ${err}`;
   }
-  //set the status of our resource to the list of pod names.
-  const status: Memcached = {
-    apiVersion: obj.apiVersion,
-    kind: obj.kind,
-    metadata: {
-      name: obj.metadata.name!,
-      resourceVersion: obj.metadata.resourceVersion,
-    },
-    status: {
-      pods: await getPodList(`deployment=${obj.metadata.name}`),
-    },
+}
+
+// Function to handle Fireline events
+async function onFirelineEvent(phase: string, apiObj: any): Promise<string> {
+  const fireline: Fireline = apiObj;
+  if (phase === 'ADDED' || phase === 'MODIFIED') {
+    return await reconcileFireline(fireline);
+  } else if (phase === 'DELETED') {
+    // Handle deletion if necessary
+    return `Fireline resource deleted: ${fireline.metadata.name}`;
+  }
+  return `Unhandled event phase: ${phase} for Fireline: ${fireline.metadata.name}`;
+}
+
+// Function to reconcile Fireline resources
+// Function to reconcile Fireline resources
+async function reconcileFireline(fireline: Fireline): Promise<string> {
+  // Example logic to determine the current stage and update stage details
+  // This should be replaced with your actual logic
+  if (!fireline.metadata || !fireline.metadata.name) {
+    return 'Error: Fireline resource name is undefined';
+  }
+  let currentStage = 'Unknown';
+  const stageDetails: StageDetail[] = [];
+
+  if (fireline.spec && fireline.spec.stages) {
+    // Example logic to set the current stage based on some conditions
+    // Replace this with your actual stage determination logic
+    currentStage = fireline.spec.stages[0]; // Placeholder for actual logic
+
+    // Populate stageDetails based on the current stage
+    // This is just an example and should be replaced with actual logic
+    stageDetails.push({
+      stageName: currentStage,
+      startTime: new Date().toISOString(),
+      status: 'InProgress', // Example status, update as per your logic
+    });
+  }
+
+  const updatedStatus: FirelineStatus = {
+    currentStage: currentStage,
+    stageDetails: stageDetails,
   };
 
-  k8sApiMC.replaceNamespacedCustomObjectStatus(
-    MEMCACHED_GROUP,
-    MEMCACHED_VERSION,
-    NAMESPACE,
-    MEMCACHED_PLURAL,
-    obj.metadata.name!,
-    status,
-  );
-}
-async function getPodList(podSelector: string): Promise<string[]> {
   try {
-    const podList = await k8sApiPods.listNamespacedPod(
-      NAMESPACE,
-      undefined,
-      undefined,
-      undefined,
-      undefined,
-      podSelector,
+    await k8sApiCustom.replaceNamespacedCustomObjectStatus(
+      'tekton.dev',
+      'v1',
+      'default', // Replace with appropriate namespace if needed
+      'firelines',
+      fireline.metadata.name,
+      { status: updatedStatus }
     );
-    return podList.body.items.map((pod) => pod.metadata!.name!);
+    return `Fireline resource reconciled: ${fireline.metadata.name}`;
   } catch (err) {
-    log(err);
+    return `Error reconciling Fireline resource: ${err}`;
   }
-  return [];
 }
 
+
+ 
+
+// Main function
 async function main() {
-  await watchResource();
-}
-
-function log(message: string) {
-  console.log(`${new Date().toLocaleString()}: ${message}`);
+  const message = await watchFirelineResource();
+  console.log(message);
 }
 
 main();
